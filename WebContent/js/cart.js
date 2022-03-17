7ca480655cb0ad9f099b1471785c3b37e09b94fd
HCL-LITES <98479712+HCL-LITES@users.noreply.github.com>
2022-03-17 14:55:14 +0530

    Add files via upload

--- WebContent/js/cart.js
+++ WebContent/js/cart.js
@@ -1,1 +1,13 @@
+function updateQty(dishName, quantity) {
+	if (quantity > 0) {
+		window.location = 'action.jsp?operator=update&dish=' + dishName + '&qty=' + quantity;
+	}
+}
 
+function remove(dishName, quantity) {
+	window.location = 'action.jsp?operator=remove&dish=' + dishName;
+}
+
+function removeAll() {
+	window.location = 'action.jsp?operator=removeAll';
+}
